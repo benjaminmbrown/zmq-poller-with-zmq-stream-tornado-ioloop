f950ee0e332aa5f5c0353afd7c3eea0d28613ce0
Benjamin M. Brown <benjaminbb@gmail.com>
2016-02-13 17:41:16 -0600

    changing SUB socket to subscriber instead of pull (missed it earlier) and updating readme

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # zmq-poller-with-zmq-stream-tornado-ioloop
 ZMQ Poller that uses ZMQStream and Torndado's ioloop for handling poll events on ZMQ sockets
 
-<br>This poller uses ZMQStream to handle polling events from ZeroMQ sockets. This also enables usage of callbacks to send & receive data
+<br>This poller uses ZMQStream to handle polling events from ZeroMQ sockets. This also enables usage of callbacks to send & receive data. There's a push server that sends out "Continue" or "Stop" messages which the worker client reads and uses to continue work or not.
 
 <br/>
 To run: python zmq-stream-poller.py
--- zmq-stream-poller.py
+++ zmq-stream-poller.py
@@ -33,10 +33,10 @@ def pub_server(port="5558"):
 	print "Created publisher server with id: ", publisher_id
 	print "Running server on port: ", port
 	for reqnum in range(100):
-		topic =  random.randrange(8,10):
+		topic =  random.randrange(7,10)
 		messagedata = "server#%s" % publisher_id
 		print "%s %s" % (topic, messagedata)
-		socket.send("%d %s" % (topic, messagedata)
+		socket.send("%d %s" % (topic, messagedata))
 		time.sleep(1)
 
 def getcommand(msg):
@@ -54,21 +54,29 @@ def client(port_push, port_sub):
 	context = zmq.Context()
 
 	socket_pull = context.socket(zmq.PULL)
-	socket_pull.connect("tcp://localhost:%s", port_push)
+	socket_pull.connect("tcp://localhost:%s" % port_push)
 	
 	#use ZMQStream class ot register callbacks - no explicit socket handlers here
 	stream_pull = zmqstream.ZMQStream(socket_pull)
 	stream_pull.on_recv(getcommand)
-	print "Connected to server on port: ", port
+	print "Connected to server on port: ", port_push
 
 
-	socket_sub = context.socket(zmq.PUSH)
- 	socket_sub.connect("tcp://localhost:%s", port_sub)
- 	socket_sub.setsockopt(zmq.SUBSCRIBE, "9")
+	socket_sub = context.socket(zmq.SUB)
+ 	socket_sub.connect("tcp://localhost:%s" % port_sub)
+ 	socket_sub.setsockopt(zmq.SUBSCRIBE,"9")
 
  	stream_sub =zmqstream.ZMQStream(socket_sub)
  	stream_sub.on_recv(process_message)
- 	
+
  	print "Connected to publisher with port #: ", port_sub
  	ioloop.IOLoop.instance().start()
  	print "Worker has stopped message processing" 
+
+
+if __name__ == "__main__":
+	server_push_port = "5556"
+	server_pub_port = "5558"
+	Process(target=push_server, args=(server_push_port,)).start()
+	Process(target=pub_server, args=(server_pub_port,)).start()
+	Process(target=client, args=(server_push_port,server_pub_port)).start()
